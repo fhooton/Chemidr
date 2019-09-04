795c90170cbc85abb4224d2160c70aebfac201a3
fhooton <yourfriendforrest@gmail.com>
2019-09-04 15:24:17 -0400

    added comments

--- chemidr/labeler.py
+++ chemidr/labeler.py
@@ -1,6 +1,16 @@
 """
     Author: Forrest Hooton
-    Purpose: Label given chemical strings with corresponding id's in popular databases
+    Purpose: Label given chemical strings with corresponding id's in popular databases (pubchem and foodb)
+
+    Primary Functions:
+        get_compound_pubchem_info: returns pubchem id and name given a string
+        append_foodb_id: adds column with foodb id's given a dataframe with chemical strings
+        append_pubchem_id: adds column with pubchem ids's given a dataframe with chemical strings
+        id_searcher: adds columns with foodb and pubchem id's, along with a composite column
+
+    Example Usage:
+        test = pd.DataFrame({'chem' : ['allicin', 'diallyl disulphide']})
+        id_searcher(test, 'chem')
 """
 
 import pandas as pd
@@ -17,11 +27,11 @@ package_path = ''.join(cwd.split('Chemidr')[:-1]) + 'Chemidr'
 
 
 # Filepath wrapper to make data load-able from Chemidr
-def make_fp(fp):
+def __make_fp__(fp):
     return f'{package_path}/{fp}'
 
 # Convert text to greet letter using *~greek letter name~*
-def greek_letter_converter(chem, convert_letter = True):
+def __greek_letter_converter__(chem, convert_letter = True):
     if convert_letter:
         chem = chem.replace('*alpha*', 'α')
         chem = chem.replace('*beta*', 'β')
@@ -37,13 +47,13 @@ def greek_letter_converter(chem, convert_letter = True):
     return chem
 
 
-def clean_term(term, convert_letter = True, w_space = True, is_url=True):
+def __clean_term__(term, convert_letter = True, w_space = True, is_url=True):
     """
         Prepares an input term to be quried in a url
 
         Input
         ----------------------------------------------------------------
-        term : string
+        term : str
             term to clean
         convert_letter : bool (default True)
             Whether or not to convert letter to greek representation
@@ -54,13 +64,13 @@ def clean_term(term, convert_letter = True, w_space = True, is_url=True):
 
         Returns
         ----------------------------------------------------------------
-        term : string
+        term : str
             same term as input after cleaning process
 
     """
     term = term.lower().strip()
     
-    term = greek_letter_converter(term, convert_letter=convert_letter)
+    term = __greek_letter_converter__(term, convert_letter=convert_letter)
     
     # Keeps spaces in string
     if w_space:
@@ -73,20 +83,20 @@ def clean_term(term, convert_letter = True, w_space = True, is_url=True):
     return term
 
 
-def retrieve_info(req):
+def __retrieve_info__(req):
     """
         retrieves pubchem synonym information and extracts the compound id and primary compound name
 
         Input
         ----------------------------------------------------------------
-        req : string
+        req : str
             chemical string to query in pubmed
 
         Returns
         ----------------------------------------------------------------
         compound_id : float
             pubchem compound id of req chemical
-        compound_name : string
+        compound_name : str
             compound name of primary compound for synonym entry
     """
     # Ex. https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/allicin/synonyms/XML
@@ -107,19 +117,17 @@ def retrieve_info(req):
 def get_compound_pubchem_info(chem):
     
     try:
-        req = clean_term(chem, convert_letter=False)
-        compound_id, compound_name = retrieve_info(req)
+        req = __clean_term__(chem, convert_letter=False)
+        compound_id, compound_name = __retrieve_info__(req)
     except:
-        req = clean_term(chem, convert_letter=False, w_space=False)
-        compound_id, compound_name = retrieve_info(req)
+        req = __clean_term__(chem, convert_letter=False, w_space=False)
+        compound_id, compound_name = __retrieve_info__(req)
     
     return compound_id, compound_name.lower()
 
 
-def check_key(val, input_dict):
-    """
-    Evaluates if a cell already contains a key. If not, and if there is a match, insert a key.
-    """
+# Evaluates if a cell already contains a key. If not, and if there is a match, insert a key
+def __check_key__(val, input_dict):
     if val is np.nan:
         if val in input_dict:
             input_dict[val]
@@ -129,24 +137,40 @@ def check_key(val, input_dict):
         return val
     
 
-# Uses the foodb synonym file to create a synonym code for a df, and potentially cross reference the synonyms
-# to another df to return what synonyms are unique
 def append_foodb_id(df, chem_key, load_ids=True):
-    
+    """
+        label chemicals with foodb id where there is a match to chemicals in foodb files under the data directory
+
+        Input
+        ----------------------------------------------------------------
+        df : pd.DataFrame
+            dataframe containing chemical names
+        chem_key : str
+            column name with the chemical strings
+        load_ids : bool (default True)
+            Whether or not to use pre-altered pubchem data. Want to use true after the program has run once to reduce runtime
+
+        Returns
+        ----------------------------------------------------------------
+        df : pd.DataFrame
+            input dataframe with a foodb_id column
+    """
     df[chem_key] = df[chem_key].str.strip().str.lower()
     
-    fdb_compounds = pd.read_csv(make_fp('data/compounds.csv'), encoding='latin1')[['id', 'name']]
+    # Read in foodb primary compound names
+    fdb_compounds = pd.read_csv(__make_fp__('data/compounds.csv'), encoding='latin1')[['id', 'name']]
     fdb_compounds.name = fdb_compounds.name.str.strip().str.lower()
     fdb_compounds.rename(columns={'id' : 'foodb_id'}, inplace=True)
     
+    # Merge on matching names
     df = df.merge(fdb_compounds, how = 'left', left_on = chem_key, right_on = 'name')
     
     if load_ids:
-        with open(make_fp('intermediate_save/fdb_synonyms.pkl'), 'rb') as f:
+        with open(__make_fp__('intermediate_save/fdb_synonyms.pkl'), 'rb') as f:
             input_dict = pickle.load(f)
     else:
         # Need to ensure document is in folder
-        compound_synonyms = pd.read_csv(make_fp('data/compound_synonymssql.csv'))
+        compound_synonyms = pd.read_csv(__make_fp__('data/compound_synonymssql.csv'))
 
         # Only keep columns with synonym and synonym id
         syn_reduced = compound_synonyms[['source_id', 'synonym']]
@@ -155,17 +179,17 @@ def append_foodb_id(df, chem_key, load_ids=True):
     
         input_dict = {row['synonym'] : row['foodb_id'] for _, row in syn_reduced.iterrows()}
         
-        with open(make_fp('intermediate_save/fdb_synonyms.pkl'), 'wb') as f:
+        with open(__make_fp__('intermediate_save/fdb_synonyms.pkl'), 'wb') as f:
             pickle.dump(input_dict, f)
 
-    df.foodb_id = df.foodb_id.apply(check_key, input_dict=input_dict)
+    df.foodb_id = df.foodb_id.apply(__check_key__, input_dict=input_dict)
     
     if load_ids:
-        with open(make_fp('intermediate_save/fdb_source_strings.pkl', 'rb')) as f:
+        with open(__make_fp__('intermediate_save/fdb_source_strings.pkl'), 'rb') as f:
             input_dict = pickle.load(f)
     else:
         # Dataframe with contents of foodb
-        foodb = pd.read_csv(make_fp('data/contentssql.csv'))
+        foodb = pd.read_csv(__make_fp__('data/contentssql.csv'))
 
         # Gets the subset of the database pertaining to garlic
         foodb = foodb[['source_id', 'orig_source_name']].drop_duplicates()
@@ -176,36 +200,52 @@ def append_foodb_id(df, chem_key, load_ids=True):
         # Creates a list of the unique chemicals in garlic from foodb
         input_dict = {row['orig_source_name'] : row['source_id'] for _, row in foodb.iterrows()}
         
-        with open(make_fp('intermediate_save/fdb_source_strings.pkl'), 'wb') as f:
+        with open(__make_fp__('intermediate_save/fdb_source_strings.pkl'), 'wb') as f:
             pickle.dump(input_dict, f)
     
-    df.foodb_id = df.foodb_id.apply(check_key, input_dict=input_dict)
+    df.foodb_id = df.foodb_id.apply(__check_key__, input_dict=input_dict)
     
     if load_ids:
-        with open(make_fp('intermediate_save/usda_ids.pkl'), 'rb') as f:
+        with open(__make_fp__('intermediate_save/usda_ids.pkl'), 'rb') as f:
             input_dict = pickle.load(f)
     else:
-        usda = pd.read_csv(make_fp('data/usda_raw_garlic.csv'), encoding = 'latin1')
+        usda = pd.read_csv(__make_fp__('data/usda_raw_garlic.csv'), encoding = 'latin1')
         usda.nut_desc = usda.nut_desc.str.strip().str.lower()
         input_dict = {row['nut_desc'] : row['chem_id'] for _, row in usda.iterrows()}
         
-        with open(make_fp('intermediate_save/usda_ids.pkl'), 'wb') as f:
+        with open(__make_fp__('intermediate_save/usda_ids.pkl'), 'wb') as f:
             pickle.dump(input_dict, f)
     
-    df.foodb_id = df.foodb_id.apply(check_key, input_dict=input_dict)
+    df.foodb_id = df.foodb_id.apply(__check_key__, input_dict=input_dict)
                 
     return df
 
 
 def append_pubchem_id(df, chem_key):
+    """
+        label chemicals with pubchem id by searching the pubchem synonyms
+
+        Input
+        ----------------------------------------------------------------
+        df : pd.DataFrame
+            dataframe containing chemical names
+        chem_key : str
+            column name with the chemical strings
+
+        Returns
+        ----------------------------------------------------------------
+        df : pd.DataFrame
+            input dataframe with a pubchem_id column
+    """
     start = time()
 
     i = 0
     for idx, row in df.iterrows():
+        # get_compound_pubchem_info() creates an error if the chemical does not exist
         try:
-        ID, name = get_compound_pubchem_info(row[chem_key])
-        df.at[idx, 'pubchem_id'] = ID
-        df.at[idx, 'pubchem_name'] = name
+            ID, name = get_compound_pubchem_info(row[chem_key])
+            df.at[idx, 'pubchem_id'] = ID
+            df.at[idx, 'pubchem_name'] = name
             
         except:
             # print(row[chem_key])
@@ -222,7 +262,25 @@ def append_pubchem_id(df, chem_key):
 
 
 def id_searcher(df, chem_key, fdb = True, pubchem = True):
-    
+    """
+        main function to assign chemical keys in pubchem and foodb
+
+        Input
+        ----------------------------------------------------------------
+        df : pd.DataFrame
+            dataframe containing chemical names
+        chem_key : str
+            column name with the chemical strings
+        fdb : bool (default True)
+            assign foodb ids
+        pubchem : bool (default True)
+            assign pubchem ids
+
+        Returns
+        ----------------------------------------------------------------
+        df : pd.DataFrame
+            input dataframe with a pubchem_id + foodb_id columns, and composite column chem_id
+    """
     if pubchem:
         df = append_pubchem_id(df, chem_key)
     if fdb:
@@ -239,6 +297,7 @@ def id_searcher(df, chem_key, fdb = True, pubchem = True):
     # Manually looked up the maximum pubchem index to make sure id's don't overlap
     max_p_index = 134825000
     
+    # If there is no pubchem_id, make chem_id the foodb_id + the maximum pubchem id
     for idx, row in df.iterrows():
         if not math.isnan(row['pubchem_id']):
             df.at[idx, 'chem_id'] = row['pubchem_id']
