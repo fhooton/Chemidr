ef1c3c25cc9ef93da55bbc08047cc8b456cd90b6
fhooton <yourfriendforrest@gmail.com>
2019-09-25 13:14:29 -0400

    added functionality

--- chemidr/id_map.py
+++ chemidr/id_map.py
@@ -1,8 +1,14 @@
 
+import numpy as np
+import math
 import urllib.request as request
+import requests
+import time
 import json
+from lxml import etree
 
-def inchikeys_from_cids(cids, as_dict=False):
+
+def cids2inchikeys(cids, as_dict=False, use_prefix=False):
 	"""
         Retrieves InChIKeys from PubChem using Pubchem CIDS
 
@@ -10,6 +16,11 @@ def inchikeys_from_cids(cids, as_dict=False):
         ----------------------------------------------------------------
         cids : list
             list of pubchem cid's for InChIKeys (needs to be ints, but also included int typecast)
+        as_dict : bool (default False)
+            returns dictionary of info if true, list otherwise
+       	use_prefix : bool (default False)
+       		only return the prefix of inchikeys (before the first -), which contains the structural information
+       		(to find out more see https://www.inchi-trust.org/technical-faq-2/)
 
         Returns
         ----------------------------------------------------------------
@@ -17,25 +28,133 @@ def inchikeys_from_cids(cids, as_dict=False):
             dictionary with CID's as keys and InChIKeys as values if as_dict is True, otherwise list
             of InChIKeys to preserve order
     """
-	cids = [str(int(i)) for i in cids]
+	cids = __divide_list__([str(int(i)) for i in cids])
 
-	# Create url for InChIKey query
-	url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{','.join(cids)}/property/InChIKey/JSON"
+	if as_dict: inchikeys = {}
+	else: inchikeys = []
 
-	with request.urlopen(url) as response:
-		r = response.read()
+	# Loop over divisions of ids to avoid overloading query
+	for ids in cids:
 
-	# option to return InChIKey's as list or as dict (dict has certainty in case some cids aren't
-	# retrieved, list preserves order)
-	if as_dict:
-		inchikeys = {
-			p['CID'] : p['InChIKey'] for p in json.loads(r)['PropertyTable']['Properties']
-		}
+		# Create url for InChIKey query
+		url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{','.join(ids)}/property/InChIKey/JSON"
 
-	else:
-		inchikeys = [
-			p['InChIKey'] for p in json.loads(r)['PropertyTable']['Properties']
-		]
+		
+		r = __safe_urlopen__(url)
+
+		# option to return InChIKey's as list or as dict (dict has certainty in case some cids aren't
+		# retrieved, list preserves order)
+		if as_dict:
+			new_dict = {
+				p['CID'] : p['InChIKey'] for p in json.loads(r)['PropertyTable']['Properties']
+			}
+			inchikeys.update(new_dict)
+
+		else:
+			new_list = [
+				p['InChIKey'] for p in json.loads(r)['PropertyTable']['Properties']
+			]
+			inchikeys += new_list
+
+	if use_prefix:
+		if isinstance(inchikeys, dict): inchikeys = {cid : ikey.split('-')[0] for cid, ikey in inchikeys.items()}
+		else: inchikeys = [ikey.split('-')[0] for ikey in inchikeys]
 
 	return inchikeys
-	
+
+
+# Divides list into even divisions with a maximum of 100 elements
+def __divide_list__(ids):
+	num_divisions = int(math.ceil(len(ids) / 100))
+
+	split_ids = np.array_split(np.asarray(ids), num_divisions)
+	split_ids = [np.ndarray.tolist(split_ids[i]) for i in range(len(split_ids))]
+
+	return split_ids
+
+
+def __safe_urlopen__(url):
+    response = requests.get(url)
+
+    if response.status_code == 200: # Successful
+        return response.content
+
+    elif response.status_code == 429: # Too many requests
+        # print('Retrying...')
+        time.sleep(.5)
+        return __safe_urlopen__(url)
+
+    elif response.status_code == 503: # PUGREST.ServerBusy
+        # print('Retrying...')
+        time.sleep(1)
+        return __safe_urlopen__(url)
+
+    elif response.status_code == 404: # PUGREST.NotFound (aka doesn't exist)
+        return None
+
+
+def mesh2pid(mesh):
+	url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pcsubstance&term={mesh}&retmode=json'
+
+	r = __safe_urlopen__(url)
+
+	if r is not None:
+		j = json.loads(r)
+
+		if j['esearchresult']['count'] != 0:
+
+			sid = j['esearchresult']['idlist'][0] # get first sid result
+
+			url = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/{sid}/xml'
+
+			xml = __safe_urlopen__(url)
+
+			try:
+				root = etree.fromstring(xml)
+
+			except:
+				print(sid)
+				print(type(xml))
+				print(xml)
+				return
+
+			cids = root.findall(".//{http://www.ncbi.nlm.nih.gov}PC-CompoundType_id_cid")
+
+			if len(cids) > 0:
+				cid = cids[0].xpath('./text()')[0]
+			else:
+				cid = np.nan
+
+			return {mesh : {'mesh' : mesh, 'sid' : sid, 'cid' : cid}}
+
+		else:
+			return {mesh : {'mesh' : mesh, 'sid' : np.nan, 'cid' : np.nan}}
+
+	else:
+		return {mesh : {'mesh' : mesh, 'sid' : np.nan, 'cid' : np.nan}}
+
+	# url = f'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pccompound&term={mesh}&retmode=json'
+
+	# r = __safe_urlopen__(url)
+
+	# if r is not None:
+	# 	j = json.reads(r)
+
+	# 	if j['esearchresult']['count'] != 0:
+
+	# 		sid = j['esearchresult']['idlist'][0] # get first sid result
+
+	# 		url = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/substance/sid/{sid}/xml'
+
+	# 		xml = __safe_urlopen__(url)
+
+	# 		root = etree.from_string(xml)
+
+	# 		cids = root.findall(".//{http://www.ncbi.nlm.nih.gov}PC-CompoundType_id_cid")
+
+	# 		if len(cids) > 0:
+	# 			cid = cids[0].xpath('./text()')[0]
+	# 		else:
+	# 			cid = np.nan
+
+	# 		return {'mesh' : mesh, 'sid' : sid, 'cid' : cid}
--- chemidr/labeler.py
+++ chemidr/labeler.py
@@ -15,12 +15,17 @@
 
 import pandas as pd
 import numpy as np
-from time import time
-import urllib.request as request
+import time
+# import urllib.request as request
+import requests
 from lxml import etree
 import json
 import math
 import pickle
+import re
+from fuzzywuzzy import fuzz
+
+from .id_map import cids2inchikeys
 
 import os
 cwd = os.path.dirname(__file__) # get current location of script
@@ -84,6 +89,20 @@ def __clean_term__(term, convert_letter = True, w_space = True, is_url=True):
     return term
 
 
+def __safe_urlopen__(url):
+    response = requests.get(url)
+
+    if response.status_code == 200: # Successful
+        return response.content
+
+    elif response.status_code == 429: # Too many requests
+        # print('Retrying...')
+        time.sleep(.5)
+        return __safe_urlopen__(url)
+
+    elif response.status_code == 404: # PUGREST.NotFound (aka doesn't exist)
+        return None
+
 def __exact_retrevial__(req):
     """
         retrieves pubchem synonym information using exact string matches and extracts the 
@@ -103,16 +122,64 @@ def __exact_retrevial__(req):
     """
     # Ex. https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/allicin/synonyms/XML
     url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{req}/synonyms/XML"
-    
-    with request.urlopen(url) as response:
-        xml = response.read()
+
+    xml = __safe_urlopen__(url)
+
+    if xml is None:
+        return np.nan, np.nan
     
     # Extracts information from xml string
-    root = root = etree.fromstring(xml)
+    root = etree.fromstring(xml)
     compound_id = float(root.findall(".//{http://pubchem.ncbi.nlm.nih.gov/pug_rest}CID")[0].xpath('.//text()')[0])
     compound_name = str(root.findall(".//{http://pubchem.ncbi.nlm.nih.gov/pug_rest}Synonym")[0].xpath('.//text()')[0])
     
     return compound_id, compound_name
+
+
+def __is_nutrient__(term):
+    general_terms = ['protein', 'fat', 'sugar', 'carbohydrate', 'fiber']
+    
+    if sum([1 for t in general_terms if t in term]) > 0:
+        return True
+    else:
+        return False
+
+
+def __clean_compound_name__(s):
+    s = s.lower()
+
+    # Remove +/- from string
+    s = re.sub('[\+\-]', '', s)
+
+    # Remove trailing s in string
+    s = re.sub('s$', '', s)
+
+    # Remove cis/trans from string
+    s = re.sub('[\s\-\(]cis[\s\-\)]|[\s\-\(]trans[\s\-\)]', '', s)
+
+    # Remove any single alpha charachters surrounded by whitespace or '-'
+    s = re.sub('[\s\-\(][a-z][\s\-\)]|^[a-z][\s\-]', '-', s)
+
+    # Replace hyphen with space
+    s = re.sub('-', ' ', s)
+
+    # Replace stretch of whitespace with space
+    s = re.sub('\s+', ' ', s)
+
+    s = s.strip()
+
+    return s
+
+def __complex_string_equivalence__(s1, s2):
+    # Remove parenthesis and anything in between
+    s1 = __clean_compound_name__(s1)
+    s2 = __clean_compound_name__(s2)
+
+    # Calculates fuzz ratio (Levenshtein Distance)
+    if fuzz.ratio(''.join(s1.split()), ''.join(s2.split())) >= 98:
+        return True
+    else:
+        return False
 
 
 # Examples https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pccompound&term=allicin&retmode=json
@@ -133,53 +200,82 @@ def __compound_search__(req):
         name : str
             compound name of primary compound for synonym entry
     """
+    req = req.lower()
+
     # Url to search for compound
     url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pccompound&term={req}&retmode=json"
 
-    with request.urlopen(url) as response:
-        r = response.read()
+    r = __safe_urlopen__(url)
+
+    if r is None:
+        return np.nan, np.nan
+
+    req = re.sub('%20', ' ', req)
 
     # Return np.nan for no search results
     if int(json.loads(r)['esearchresult']['count']) == 0:
-        return np.nan, req
+        return np.nan, np.nan
 
-    cid = json.loads(r)['esearchresult']['idlist'][0]
+    cid = float(json.loads(r)['esearchresult']['idlist'][0])
 
     # Url to get the name associated with a cid
     name_url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{cid}/synonyms/json"
-    name = json.loads(request.urlopen(name_url).read())['InformationList']['Information'][0]['Synonym'][0]
 
-    # Return the cid and name if the search retrieves 1 result
+    # a cid did not have synonyms for some reason
+    jsn = __safe_urlopen__(name_url)
+
+    if jsn is None:
+        return np.nan, np.nan
+
+    name = json.loads(jsn)['InformationList']['Information'][0]['Synonym'][0]
+
+    # Return the cid and name if the search retrieves 1 result and passes string equivalence
     if int(json.loads(r)['esearchresult']['count']) == 1:
-        return cid, name
-
-    # Returns cid and name if the strings (first result, req) match is >= 98
-    elif int(json.loads(r)['esearchresult']['count']) > 1:
-        s1 = name.replace('(', '').replace(')', '').lower()
-        s2 = req.replace('(', '').replace(')', '').lower()
-
-        # Calculates fuzz ratio (Levenshtein Distance)
-        if fuzz.ratio >= 98:
+        if __complex_string_equivalence__(req, name) == True:
             return cid, name
         else:
-            return np.nan, req
+            return np.nan, np.nan
+
+    # Returns cid and name if first result passes the complex equivalence test with the req
+    elif int(json.loads(r)['esearchresult']['count']) > 1:
+        if __complex_string_equivalence__(req, name) == True:
+            return cid, name
+        else:
+            return np.nan, np.nan
 
 
 # try-except wrapper to retrieve pubchem info
 def get_compound_pubchem_info(chem):
+    if __is_nutrient__(chem):
+        return np.nan, chem
+        
+    # try:
+    req = __clean_term__(chem, convert_letter=False)
+    compound_id, compound_name = __exact_retrevial__(req)
     
-    try:
-        req = __clean_term__(chem, convert_letter=False)
-        compound_id, compound_name = __exact_retrevial__(req)
-    except:
-        try:
-            req = __clean_term__(chem, convert_letter=False, w_space=False)
-            compound_id, compound_name = __exact_retrevial__(req)
-        except:
-            req = __clean_term__(chem, convert_letter=False)
-            compound_id, compound_name = __compound_search__(req)
+    if not math.isnan(compound_id):
+        return compound_id, compound_name
+    # except:
+        # try:
+    req = __clean_term__(chem, convert_letter=False, w_space=False)
+    compound_id, compound_name = __exact_retrevial__(req)
+        # except:
+            # pass
+            # try:
+    if not math.isnan(compound_id):
+        return compound_id, compound_name
+
+    req = __clean_term__(chem, convert_letter=False)
+    compound_id, compound_name = __compound_search__(req)
+            # except:
     
-    return compound_id, compound_name.lower()
+    if not math.isnan(compound_id):
+        return compound_id, compound_name
+
+    req = __clean_term__(__clean_compound_name__(chem), convert_letter=False)
+    compound_id, compound_name = __compound_search__(req)
+
+    return compound_id, compound_name
 
 
 # Evaluates if a cell already contains a key. If not, and if there is a match, insert a key
@@ -293,31 +389,62 @@ def append_pubchem_id(df, chem_key):
         df : pd.DataFrame
             input dataframe with a pubchem_id column
     """
-    start = time()
+    start = time.time()
 
     i = 0
     for idx, row in df.iterrows():
         # get_compound_pubchem_info() creates an error if the chemical does not exist
-        try:
-            ID, name = get_compound_pubchem_info(row[chem_key])
-            df.at[idx, 'pubchem_id'] = ID
-            df.at[idx, 'pubchem_name'] = name
+        # try:
+        ID, name = get_compound_pubchem_info(row[chem_key])
+        df.at[idx, 'pubchem_id'] = ID
+        df.at[idx, 'pubchem_name'] = name
             
-        except:
+        # except:
             # print(row[chem_key])
-            pass
+            # pass
         
         if not i % 1000:
-            print(idx, 'chems searched in', (time() - start) / 60, "min")
+            print(idx, 'chems searched in', (time.time() - start) / 60, "min")
 
         i += 1
 
-    print("Pubchem ids added in", (time() - start) / 60, "min")
+    print("Pubchem ids added in", (time.time() - start) / 60, "min")
+
+    return df
+
+def __darkmatter_database__(df, chem_key):
+    dmdb = pd.read_csv(__make_fp__('data/DarkMatter Databases.csv'))
+
+    dmdb.name = dmdb.name.str.lower()
+    dmdb.USDA = dmdb.USDA.str.lower()
+
+    dmdb.Pubchem = dmdb.Pubchem.replace('0', np.nan)
+    dmdb['FooDB ID'] = dmdb['FooDB ID'].replace('0', np.nan)
+
+    for idx, row in df.iterrows():
+        if np.isnan(row['pubchem_id']):
+            if row[chem_key] in dmdb['name'].tolist():
+                df.at[idx, 'pubchem_id'] = float(dmdb[dmdb['name'] == row[chem_key]].Pubchem.tolist()[0])
+                df.at[idx, 'inchikey'] = dmdb[dmdb['name'] == row[chem_key]].start.tolist()[0]
+                df.at[idx, 'foodb_id'] = float(dmdb[dmdb['name'] == row[chem_key]]['FooDB ID'].tolist()[0])
+
+            elif row[chem_key] in dmdb['USDA'].tolist():
+                df.at[idx, 'pubchem_id'] = float(dmdb[dmdb['USDA'] == row[chem_key]].Pubchem.tolist()[0])
+                df.at[idx, 'inchikey'] = dmdb[dmdb['USDA'] == row[chem_key]].start.tolist()[0]
+                df.at[idx, 'foodb_id'] = float(dmdb[dmdb['USDA'] == row[chem_key]]['FooDB ID'].tolist()[0])
+
+        elif np.isnan(row['foodb_id']):
+            if row[chem_key] in dmdb['name'].tolist():
+                df.at[idx, 'foodb_id'] = float(dmdb[dmdb['name'] == row[chem_key]]['FooDB ID'].tolist()[0])
+
+            elif row[chem_key] in dmdb['USDA'].tolist():
+                df.at[idx, 'foodb_id'] = float(dmdb[dmdb['USDA'] == row[chem_key]]['FooDB ID'].tolist()[0])
 
     return df
 
 
-def id_searcher(df, chem_key, fdb = True, pubchem = True):
+
+def id_searcher(df, chem_key, fdb = True, pubchem = True, use_prefix=True):
     """
         main function to assign chemical keys in pubchem and foodb
 
@@ -331,6 +458,9 @@ def id_searcher(df, chem_key, fdb = True, pubchem = True):
             assign foodb ids
         pubchem : bool (default True)
             assign pubchem ids
+        use_prefix : bool (default True)
+            only return the prefix of inchikeys (before the first -), which contains the structural information
+            (to find out more see https://www.inchi-trust.org/technical-faq-2/)
 
         Returns
         ----------------------------------------------------------------
@@ -351,17 +481,25 @@ def id_searcher(df, chem_key, fdb = True, pubchem = True):
     
     # num_covered = len(df[df.foodb_id.notnull()].foodb_id.drop_duplicates())
     # print('FooDB unique compound coverage', num_covered / total, '%')
+
+    df.at[df[df.pubchem_id.notnull()].index, 'inchikey'] = cids2inchikeys(df[df.pubchem_id.notnull()].pubchem_id.tolist(), use_prefix=use_prefix)
     
+    df = __darkmatter_database__(df, chem_key)
+
     # Manually looked up the maximum pubchem index to make sure id's don't overlap
     max_p_index = 134825000
+
+    # Ensure that column type is object
+    df['chem_id'] = np.nan
+    df['chem_id'] = df['chem_id'].astype(object)
     
     # If there is no pubchem_id, make chem_id the foodb_id + the maximum pubchem id
     for idx, row in df.iterrows():
-        if not math.isnan(row['pubchem_id']):
-            df.at[idx, 'chem_id'] = row['pubchem_id']
+        # if not math.isnan(row['inchikey']):
+        if isinstance(row['inchikey'], str):
+            df.at[idx, 'chem_id'] = row['inchikey']
         else:
             df.at[idx, 'chem_id'] = row['foodb_id'] + max_p_index
-    print(len(df))
     
     # num_covered = len(df[df.chem_id.notnull()].chem_id.drop_duplicates())
     # print('Total unique compound covereage', num_covered / total, '%')
